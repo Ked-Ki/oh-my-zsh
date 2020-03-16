a9149d1ebb503860d683d7daca31803a1038ff5a
Kelsey Freese <kmfreese@protonmail.com>
2020-03-16 14:29:40 -0500

    remove todo.sh aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -25,28 +25,9 @@ alias sl=ls
 alias acc='list_dsnet.sh -a'
 alias ss='list_dsnet.sh -s'
 alias init='list_dsnet.sh -i'
+alias dsnum='list_dsnet.sh -d'
 
 alias git-remove-untracked='git fetch --prune && git branch -r | awk "{print \$1}" | egrep -v -f /dev/fd/0 <(git branch -vv | grep origin) | awk "{print \$1}" | xargs git branch -d'
 
-if [ "$(hostname)" = "raven.local" ]; then
-  # raven only
-  alias tlch='todo.sh lsgc @work | less'
-  alias tlph='todo.sh lsgp @work | less'
-elif [ "$(hostname)" = "lenore" ]; then
-  # lenore only
-  alias tlch='todo.sh lsgc @home | less'
-  alias tlph='todo.sh lsgp @home | less'
-fi
-
-# todo
-alias t=todo.sh
-alias ta='todo.sh add'
-alias td='todo.sh do'
-alias tl='todo.sh list'
-alias tlc='todo.sh lsgc'
-alias tlp='todo.sh lsgp'
-alias ts='todo.sh pom start'
-alias tpr='todo.sh pom_report'
-alias tmtg='ts $(tl +ongoing meetings | head -1 | cut -d" " -f1)'
-alias tslk='ts $(tl +ongoing slack | head -1 | cut -d" " -f1)'
-alias tqst='ts $(tl +ongoing questions | head -1 | cut -d" " -f1)'
+alias lab_notes='vim ~/lab_notes.md'
+alias pom='noti pomodoro'
